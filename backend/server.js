fb7fe62190bd86442e4b34664cd389d4fcbac691
Harsh Naagar <140304256+HarshNaagar77@users.noreply.github.com>
2025-10-27 21:13:06 +0530

    Delete backend directory

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-const app = require("./src/app.js")
-
-
-app.listen(3000, (req, res)=>{
-    console.log("Server is running on port 3000")
-})
--- backend/src/routes/ai.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-
-const router  = express.Router()
-
-const aiController = require("../controllers/ai.controllers")
-
-router.post("/get-review", aiController.getResponse)
-
-module.exports = router
